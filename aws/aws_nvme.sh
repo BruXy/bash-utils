eeb0543aed4f1624e9e2322d9e12daa499ff1677
Martin Bruchanov <bruchy@gmail.com>
2021-07-28 15:36:43 -0300

    NVMe listing tool recognises ephemeral disks now

--- aws/aws_nvme.sh
+++ aws/aws_nvme.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+#
+# Display AWS device mappings
+#
+
+# Check for dependencies
+nvme version > /dev/null || {
+    printf "Please install 'nvme-cli' package first!\n"
+    exit 1
+}
+
+# Display devices
 ( printf "AWS console;Volume Id;Local device\n"
 for nvme_dev in $(nvme list | grep -Eo '/dev/nvme[0-9]n[0-9](p[0-9])?' | grep -v 'p[0-9]$')
 do
     aws_dev=$(nvme id-ctrl -v $nvme_dev | grep '^0000' | grep -Eo '(/dev/)?[a-z]{3,4}')
-    aws_id=$(nvme id-ctrl -v $nvme_dev | sed -ne '/^sn/{s/.*vol\([[:xdigit:]]*\)/vol-\1/;p}')
+    aws_id=$(nvme id-ctrl -v $nvme_dev | sed -ne '/^sn/{s/.*: //;p}')
 
-    if [[ $aws_dev != /dev/* ]] ; then
+    if [[ $aws_id == vol* ]] ; then
+        aws_id=${aws_id/vol/vol-}
+    else
+        aws_dev="ephemeral" # volume id should start with 'AWS'
+    fi
+
+    if [[ $aws_dev != "ephemeral" ]] && [[ $aws_dev != /dev/* ]] ; then
         aws_dev=/dev/$aws_dev
     fi
     printf "%s;%s;%s\n" $aws_dev $aws_id $nvme_dev
