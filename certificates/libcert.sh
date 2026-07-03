ef7d551a77e00258e585308f88a1e417c4afb498
Martin Bruchanov <bruchy@gmail.com>
2026-07-03 09:17:18 +0200

    Script for checking expiration of given hostnames

---
+++ certificates/libcert.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#set -euo pipefail
+
+DATE_FMT='%Y-%b-%d %A'
+
+# %Y ... Year (e.g. 2024)
+# %b ... Short months name (e.g. Nov)
+# %d ... Day of month (01-31)
+# %A ... Full weekday name (e.g. Friday)
+
+# MacOS uses LibreSSL by default. We install the latest OpenSSL version through brew and use it
+[[ -f "/usr/local/opt/openssl/bin/openssl" ]] && \
+    export OPENSSL_BIN=/usr/local/opt/openssl/bin/openssl || \
+    export OPENSSL_BIN=/usr/bin/openssl
+
+function unix2days() {
+    local unix_time=$1
+
+    # Truncate UNIX time to 10 bytes to cut miliseconds if present
+    unix_time=${unix_time:0:10}
+    today_unix=$(date +%s)
+    days_to_expiration=$[(unix_time - today_unix)/(24*3600)]
+    not_after=$(date -d @${unix_time} "+$DATE_FMT")
+
+    if [ $days_to_expiration -gt 0 ] ; then
+        printf "In %d days (%s)." $days_to_expiration "$not_after"
+    else
+        printf "Already expired on: %s." "$not_after"
+    fi
+}
+
+function get_raw_certificate() {
+    local host=$1
+
+    # Add :443 only if no port is specified at the end
+    [[ ! "$host" =~ :[0-9]+$ ]] && host="${host}:443"
+
+    $OPENSSL_BIN s_client -showcerts -connect $host </dev/null 2>/dev/null |\
+    sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p'
+
+    if [ $? -ne 0 ] ; then
+        printf "Error: Could not retrieve certificate from %s\n" "$host"
+        return 1
+    fi
+}
+
+function get_issuer() {
+    local input=${1:-/dev/stdin}
+    
+    $OPENSSL_BIN x509 -noout -issuer -nameopt RFC2253 -in $input |\
+        sed -e 's/issuer=//' | sed -e 's/[A-Z]*=//g' | sed -e 's/,/, /g'
+}
+
+function check_expiration() {
+    local input=${1:-/dev/stdin}
+
+    not_after=$(${OPENSSL_BIN} x509 -noout -enddate -in $input |\
+         sed -e 's/notAfter=//')
+
+    unix2days $(date +%s -d "$not_after")
+}
+
+function time_unix2date() {
+    local unix_time=$1
+    # Truncate UNIX time to 10 bytes to cut miliseconds if present
+    not_after=$(date -d @${unix_time:0:10})
+    printf "%s" "$not_after"
+}
+
+
+function get_modulus_cert() {
+    local input=${1:-/dev/stdin}
+
+    $OPENSSL_BIN x509 -modulus -noout -in $input |\
+        $OPENSSL_BIN md5 | sed 's/MD5(stdin)= //'
+}
+
+function get_modulus_key() {
+    local input=${1:-/dev/stdin}
+
+    $OPENSSL_BIN rsa -modulus -noout -in $input |\
+        $OPENSSL_BIN md5 | sed 's/MD5(stdin)= //'
+}
+
+function modulus_equal() {
+    local cert_modulo=$1
+    local key_modulo=$2
+
+    if [[ "$cert_modulo" != "$key_modulo" ]] ; then
+        printf "Certificate and private key does not match!\n"
+        echo "Cert: $cert_modulo"
+        echo "Key: $key_modulo"
+        return 1
+    fi
+}
